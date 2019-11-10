825fd278bc1310e5cb3a339646f4745fdbe43d8b
Rlpcap <rafael.lopezpisani@davinci.edu.ar>
2019-11-10 20:48:43 -0300

    Lo de la primera clase
    Hice lo de la primera clase, pero no entendí qué hacer con lo de popup de herencias de state.

---
+++ Easy_State_Machine/Assets/Editor/EZStateMachineEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(EZStateMachine))]
+public class EZStateMachineEditor : Editor
+{
+    EZStateMachine _ezSM;
+    string _name;
+
+    private void OnEnable()
+    {
+        _ezSM = (EZStateMachine)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (_ezSM.mySM == null)
+        {
+            EditorGUILayout.Space();
+            _name = EditorGUILayout.TextField("Name: StateMachine_", _name);
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Create and asign StateMachine"))
+            {
+                if (!AssetDatabase.IsValidFolder("Assets/StateMachines"))
+                    AssetDatabase.CreateFolder("Assets", "StateMachines");
+                StateMachineScriptable stateMachine = ScriptableObjectUtility.CreateAsset<StateMachineScriptable>("Assets/StateMachines/", "StateMachine_" + _name);
+                _ezSM.mySM = stateMachine;
+                StateNodesBase.OpenWindow();
+                EditorUtility.SetDirty(stateMachine);
+                EditorUtility.SetDirty(_ezSM.mySM);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
+        }
+    }    
+}
